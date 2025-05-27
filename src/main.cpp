6c2d813b97e5f5ff1e6d4f8ccc2cd390af167680
Kosekans <lkruyssee@gmail.com>
2025-05-27 18:06:30 +0200

    Remove redundant comments for NeoPixel object declarations in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -84,15 +84,6 @@ const unsigned long sensorInterval = 2000;
 #define LED_COUNT_STRIP1 82 
 #define LED_COUNT_STRIP2 82
 
-// Declare our NeoPixel objects:
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 Adafruit_NeoPixel strip1(LED_COUNT_STRIP1, LED_PIN_STRIP1, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel strip2(LED_COUNT_STRIP2, LED_PIN_STRIP2, NEO_GRB + NEO_KHZ800);
 
